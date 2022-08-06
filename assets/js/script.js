8b34768dfb1e6f59a38ed90f952f18dfa22987d1
arif123456 <arif13414@gmail.com>
2022-08-06 23:43:01 +0600

    Aded category toggle of sidebar

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,15 @@
       $(this).next().slideToggle();
       
     })
+
+    categoryToggle = $('.widget .children');
+    categoryToggle.hide();
+    categoryToggle.before('<span class="cat-toggle-icon"><i class="fa-solid fa-angle-down"></i></span>');
+    hangleIcon = $('.cat-toggle-icon');
+    hangleIcon.on('click', function() {
+      $(this).toggleClass('active');
+      $(this).next().slideToggle();
+    })
   })
 
 })(jQuery);
