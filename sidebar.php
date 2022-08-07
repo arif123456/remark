13a7ce2a05ee58f9b0cb40362b301d950647a521
arif123456 <arif13414@gmail.com>
2022-08-07 17:53:16 +0600

    Added default sidebar

--- sidebar.php
+++ sidebar.php
@@ -7,9 +7,21 @@
  * @package remark
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
+	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+		?>
+			<aside id="secondary" class="widget-area">
+				<section class="widget">
+					<?php get_search_form(); ?>
+				</section>
+				<section class="widget">
+					<h2 class="widget-title"><?php esc_html_e( 'Archives', 'remark' ); ?></h2>
+					<ul>
+						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+					</ul>
+				</section>
+			</aside>
+		<?php
+	}
 ?>
 
 <aside id="secondary" class="widget-area">
